fc1c226e98f8b2677e24d58270ee41006df4b607
snehaldighore21 <snehaldighore21@gmail.com>
2026-06-10 19:02:44 +0530

    update

--- infrastructure/backend/variables.tf
+++ infrastructure/backend/variables.tf
@@ -3,7 +3,7 @@
 variable "aws_region" {
   description = "AWS region for VPC and EKS."
   type        = string
-  default     = "us-east-1"
+  default     = "ap-southeast-2"
 }
 
 variable "environment" {
